87d6e1f77e82afa7331aefc45991fa8737d97ca0
Francis <fhrancorey99@gmail.com>
2024-03-09 11:41:09 +0100

    feat: added cors headers

--- biodata_project/settings.py
+++ biodata_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'django_filters',
+    'corsheaders',
     # local app
     'api.apps.ApiConfig',
 ]
@@ -55,12 +56,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://biodata-portal-frontend.vercel.app/',
 ]
 
 ROOT_URLCONF = 'biodata_project.urls'
